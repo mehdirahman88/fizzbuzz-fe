3b9f66ece6e0715bb11b555bc0be19df931f4fdb
mehdirahman88 <hide.m88@gmail.com>
2023-03-16 02:58:56 +0700

    Add test for fizzbuzz page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,56 @@
+import axios from 'axios';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor, act} from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('FizzBuzz Page',() => {
+    let response;
+
+    jest.mock('axios');
+
+    beforeEach(() => {
+        let response = {
+            isSuccess: true,
+            data: {
+                data: "-",
+            }
+        }
+
+        axios.get=  jest.fn().mockResolvedValue( Promise.resolve(response) );
+
+    })
+
+    it('should render correctly first time', async () => {
+        render(<App />);
+
+        expect(screen.getByText(/Your count/i)).toBeInTheDocument();
+        expect(screen.getByText(/2/i)).toBeInTheDocument();
+        expect(screen.getByText(/CLICK ME/i)).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByText("-")).toBeInTheDocument();
+        });
+    })
+
+    it('should increment counter after button click', async () => {
+        render(<App />);
+
+        const fizzValue = "Fizz";
+        response = {
+            isSuccess: true,
+            data: {
+                data: fizzValue,
+            }
+        }
+        axios.get=  jest.fn().mockResolvedValue( Promise.resolve(response) );
+
+        const button = screen.getByRole('button');
+        userEvent.click(button);
+
+        expect(screen.queryByText(/2/i)).not.toBeInTheDocument();
+        expect(screen.getByText(/3/i)).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByText(fizzValue)).toBeInTheDocument();
+        });
+    })
+})
